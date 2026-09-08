05fee82cbc6a9307ad883cc756d78b835ea24882
promaray <proma.ray@gmail.com>
2026-09-07 21:34:33 -0400

    First Commit

---
+++ mcp_server.py
@@ -0,0 +1,81 @@
+from typing import List
+
+from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.prompts import base
+
+from pydantic import Field
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment."
+}
+
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description="The ID of the document to read.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
+@mcp.tool(
+    name="edit_doc_contents",
+    description="Edit the contents of a document and return the updated content."
+)
+def edit_document(
+    doc_id: str = Field(description="The ID of the document that will be edited."),
+    old_str: str = Field(description="The string to be replaced in the document."),
+    new_str: str = Field(description="The string to replace the old string with.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
+@mcp.resource(
+    "docs://documents", 
+    mime_type="application/json"
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+@mcp.resource(
+    "docs://documents/{doc_id}", 
+    mime_type="text/plain"
+)
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
+@mcp.prompt(
+    name = "format",
+    description = "Rewrite the contents of a document in a markdown format.",
+)
+def format_document(
+    doc_id:str = Field(description="The ID of the document to format.")
+) -> list[base.Message]:
+    prompt = f"""
+    Your goal is to reformat the contents of the document into a markdown format.
+    The id of the document you need to reformat is:
+     <document_id>
+       {doc_id}
+    </document_id>
+   Add in headers, subheaders, and bullet points where appropriate. 
+   Use the 'edit_document' tool to make any changes to the document's contents.
+   """
+    return [
+        base.UserMessage(prompt)
+    ]
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
